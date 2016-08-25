46c2ddd2bfdb6523a22b51425d2b945b2fd762e3
Pudding <aideen@aideen.pw>
2016-08-25 15:58:04 -0700

    Test setter three times for good measure

--- test/properties.js
+++ test/properties.js
@@ -1,11 +1,19 @@
 const tape = require('tape'), mew = require('../lib')
 
 tape('setter works', t => {
-  t.plan(1)
-  let expected = Symbol()
+  t.plan(3)
+  let expected = Symbol('1')
   let fn = a => {}
   fn.setter = actual => t.is(actual, expected, 'equivalence')
   mew('__test__', fn)
+
   mew.__test__ = expected
+
+  expected = Symbol('2')
+  mew.__test__ = expected
+
+  expected = Symbol('3')
+  mew.__test__ = expected
+
   delete mew.__test__
 })
